974661a67b3fae14447cef89450eedbe59908995
SebixDev <sebastian.nefzger@t-online.de>
2026-04-21 00:14:58 +0200

    feat: implement basic search button functionality

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 const searchInput = document.querySelector(".search-box input");
 const searchButton = document.querySelector(".search-box button");
 const tempDisplay = document.querySelector("#temp-display");
+
+// Button-Eventlistener hinzugefügt
+searchButton.addEventListener("click", () => {
+    console.log("Button wurde geklickt!");
+    alert("Suche gestartet für: " + searchInput.value);
+});
