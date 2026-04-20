b0da73b28e2e85f2bd295eeb98ae996276b62b0b
SebixDev <sebastian.nefzger@t-online.de>
2026-04-21 00:09:29 +0200

    feat: fetch UI components in script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Bausteine aus dem HTML ins JavaScript geholt 
+const searchInput = document.querySelector(".search-box input");
+const searchButton = document.querySelector(".search-box button");
+const tempDisplay = document.querySelector("#temp-display");
