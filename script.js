1f79476f3c9808256b35344bd2a4211a1bcc9eb2
SebixDev <sebastian.nefzger@t-online.de>
2026-04-22 15:07:53 +0200

    docs: update and clarify code comments

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-/* 1. VARIABLEN (Zutaten & Adressen) */
+/* 1. VARIABLEN */
 const apiKey = "DEIN_API_KEY_HIER";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchInput = document.querySelector(".search-box input");
 const searchButton = document.querySelector(".search-box button");
 
-/* 2. FUNKTIONEN (Die Baupläne) */
+/* 2. FUNKTIONEN */
 async function checkWeather(city) {
-    // Der Code schickt eine Anfrage an den Wetter-Server und wartet, bis das Datenpaket zurückkommt:
+    // Anfrage an den Wetter-Server
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     
-    // Wir wandeln das Paket in ein lesbares Format (JSON) um
+    // Paket in ein lesbares Format (JSON) umgewandelt
     var data = await response.json();
 
-    // Zum Testen: Das Paket in der Browser-Konsole (F12) anzeigen
+    // Paket in der Browser-Konsole (F12) anzeigen
     console.log(data);
 
     // Die echten Daten in die HTML-Elemente schreiben
@@ -22,14 +22,14 @@ async function checkWeather(city) {
     document.querySelector("#description").innerHTML = data.weather[0].description;
 }
 
-/* 3. EVENTS (Der Startschuss) */
+/* 3. EVENTS */
 
-// Wenn man auf die Lupe/den Button klickt
+// Button-Klick
 searchButton.addEventListener("click", () => {
     checkWeather(searchInput.value);
 });
 
-// Wenn man im Textfeld die Enter-Taste drückt
+// Textfeld: Enter-Taste 
 searchInput.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
         checkWeather(searchInput.value);
