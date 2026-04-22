f329b08d375fbfcc57cfe998e9e5ebdbc601a17d
SebixDev <sebastian.nefzger@t-online.de>
2026-04-22 15:00:25 +0200

    feat: add checkWeather function to fetch and display data

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ searchButton.addEventListener("click", () => {
     console.log("Button wurde geklickt!");
     alert("Suche gestartet für: " + searchInput.value);
 });
+
+/* 2. FUNKTIONEN (Die Baupläne) */
+async function checkWeather(city) {
+    // Der Code schickt eine Anfrage an den Wetter-Server und wartet, bis das Datenpaket zurückkommt:
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    
+    // Wir wandeln das Paket in ein lesbares Format (JSON) um
+    var data = await response.json();
+
+    // Zum Testen: Das Paket in der Browser-Konsole (F12) anzeigen
+    console.log(data);
+
+    // Die echten Daten in die HTML-Elemente schreiben
+    document.querySelector("#city-name").innerHTML = data.name;
+    document.querySelector("#temp-display").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector("#description").innerHTML = data.weather[0].description;
+}
